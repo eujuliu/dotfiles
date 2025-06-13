898cfbeb60f6685ba26ebfb85d01c5e20f83822e
Julio Martins <juliomarttins5@gmail.com>
2025-06-13 13:05:58 -0300

    feat: add television finder to zed

--- fish/config.fish
+++ fish/config.fish
@@ -16,3 +16,8 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/julio/.lmstudio/bin
+# End of LM Studio CLI section
+
