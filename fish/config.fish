9f06c1ae4dbab9635392e636b6d9063197a199ef
Julio Martins <juliomarttins5@gmail.com>
2025-08-18 09:16:31 -0300

    feat: change television shortcut into zed

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux PYENV_ROOT $HOME/.pyenv
-fish_add_path $PYENV_ROOT/bin
-
 fish_add_path $HOME/.local/bin
 
 starship init fish | source
-pyenv init - fish | source
 
 # pnpm
 set -gx PNPM_HOME "/home/julio/.local/share/pnpm"
@@ -16,8 +12,3 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/julio/.lmstudio/bin
-# End of LM Studio CLI section
-
---
+++ fish/functions/zed.fish
@@ -0,0 +1,4 @@
+function zed --wraps=/usr/bin/zeditor --description 'alias zed=/usr/bin/zeditor'
+  /usr/bin/zeditor $argv
+        
+end
