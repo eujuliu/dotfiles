20bea722fa2d0f0a830cce24adcc4ffd969c7d4c
Julio Martins <contact.juliomartins@gmail.com>
2026-07-06 19:05:29 -0300

    feat: updated tmux config

--- fish/functions/daily.fish
+++ fish/functions/daily.fish
@@ -1,3 +1,3 @@
 function daily --description 'Import Daily Markdown tasks to Taskwarrior'
-    taskmd "/mnt/wwn-0x5001b448b5de76a7-part3/Notes/Notas/Daily.md" Daily
+    taskmd "/mnt/wwn-0x5001b448b5de76a7-part3/Notes/Notas/Daily.md" Daily $argv
 end
